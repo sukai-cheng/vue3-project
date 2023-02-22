f833bdb4d4ca9b0cf2b4b6627d7a7e312dff6fa4
sukai-cheng <chengsukai@gmail.com>
2023-02-22 23:13:13 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,61 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="container">
+    <column-list :list="list"></column-list>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+import ColumnList, { ColumnProps } from '@/components/ColumnList.vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+// mock数据
+const testData: ColumnProps[] = [
+  {
+    id: 1,
+    title: 'test1的专栏',
+    description: '这是test1的专栏，有一段非常有意思的简介，可以更新一下欧',
+    avatar: 'https://chengsukai.oss-cn-hangzhou.aliyuncs.com/2022-10-11/26466404-eacd-4741-8353-93b38586c905_e3284f319e256a5d.jpg'
+  },
+  {
+    id: 2,
+    title: 'test2的专栏',
+    description: '这是test2的专栏，有一段非常有意思的简介，可以更新一下欧',
+    avatar: 'https://chengsukai.oss-cn-hangzhou.aliyuncs.com/2022-10-11/26466404-eacd-4741-8353-93b38586c905_e3284f319e256a5d.jpg'
+  },
+  {
+    id: 3,
+    title: 'test3的专栏',
+    description: '这是test3的专栏，有一段非常有意思的简介，可以更新一下欧',
+    avatar: 'https://chengsukai.oss-cn-hangzhou.aliyuncs.com/2022-10-11/26466404-eacd-4741-8353-93b38586c905_e3284f319e256a5d.jpg'
+  },
+  {
+    id: 4,
+    title: 'test4的专栏',
+    description: '这是test4的专栏，有一段非常有意思的简介，可以更新一下欧',
+    avatar: 'https://chengsukai.oss-cn-hangzhou.aliyuncs.com/2022-10-11/26466404-eacd-4741-8353-93b38586c905_e3284f319e256a5d.jpg'
+  },
+  {
+    id: 5,
+    title: 'test5的专栏',
+    description: '这是test5的专栏，有一段非常有意思的简介，可以更新一下欧',
+    avatar: 'https://chengsukai.oss-cn-hangzhou.aliyuncs.com/2022-10-11/26466404-eacd-4741-8353-93b38586c905_e3284f319e256a5d.jpg'
+  }
+]
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    ColumnList
+  },
+  setup () {
+    return {
+      list: testData
+    }
   }
 })
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/ColumnList.vue
@@ -0,0 +1,50 @@
+<template>
+  <ul>
+    <li v-for="column in list" :key="column.id">
+      <img :src="column.avatar" :alt="column.title">
+      <h5>{{ column.title }}</h5>
+      <p>{{ column.description }}</p>
+      <a href="#">进入专栏</a>
+    </li>
+  </ul>
+</template>
+
+<script lang="ts">
+
+import { defineComponent, PropType } from 'vue'
+
+export interface ColumnProps {
+  id: number,
+  title: string,
+  avatar: string,
+  description: string
+}
+
+export default defineComponent({
+  // 名字
+  name: 'ColumnList',
+  // 属性
+  props: {
+    list: {
+      // 构造函数断言成一个类型
+      type: Array as PropType<ColumnProps[]>,
+      required: true
+    }
+  },
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  setup (props) {
+
+  }
+})
+
+</script>
+
+<style scoped>
+ul > li {
+  list-style-type: none;
+}
+ul > li > img {
+  width: 150px;
+  height: 150px;
+}
+</style>
